ee18f72cf59deda7eb5a595c26836a02917e856d
Barnabas27 <bnyambura032@gmail.com>
2022-07-21 06:12:05 +0300

    add loading spinner

--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,5 +1,5 @@
 import { useEffect,useState } from "react"
-
+import Spinner from "../layout/Spinner"
 function UserList() {
   const [users, setUsers] = useState([])
   const [loading,setLoading] = useState(true)
@@ -17,12 +17,16 @@ function UserList() {
     setUsers(data)
     setLoading(false)
   }
-
+  if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
       {users.map((user) => (<h3>{user.login}</h3>))}
       </div>
     )
+  } else {
+    return < Spinner />
+  }
+    
     
       
 }
