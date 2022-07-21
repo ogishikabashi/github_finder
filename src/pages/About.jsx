5028c26b4eeb267af837751ec96dff547968434e
Barnabas27 <bnyambura032@gmail.com>
2022-07-21 05:57:42 +0300

    add pages about, home and NotFound

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+        <h1 className="mb-4 text-2xl font-light">A React app to search GitHub profiles and displaying profile details.</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+ import UserList from "../components/users/UserList"
+function Home() {
+   
+  return (
+    <>
+      
+        <UserList />
+     
+    </>
+  )
+}
+
+export default Home
