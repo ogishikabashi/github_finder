4dfd15b9f80198f27ef597462ca6eb855dd9bfb7
Barnabas27 <bnyambura032@gmail.com>
2022-07-21 10:08:31 +0300

    add user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
  import UserList from "../components/users/UserList"
+ import UserSearch from "../components/users/UserSearch"
 function Home() {
    
   return (
     <>
-      
+        <UserSearch />
         <UserList />
      
     </>
