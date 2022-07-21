c73c947af2853eeea4ee6f174f3f0f2d9b71b530
Barnabas27 <bnyambura032@gmail.com>
2022-07-21 09:11:12 +0300

    add clean up on fetch users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,13 +9,16 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true
+    loading: false
   }
 
   //destructuring
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
+// no use(testing though)
   const fetchUsers = async () => {
+
+    setLoading()
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -27,6 +30,10 @@ export const GithubProvider = ({ children }) => {
         payload: data,
     })
   };
+
+  //set loading function
+
+  const setLoading = () => dispatch({type: 'SET_LOADING'})
   return (
     <GithubContext.Provider
       value={{
